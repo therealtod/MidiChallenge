3192639ab18e348b5505d0f41206ecafb69f860a
Francesco Fricano <fr.fricano@gmail.com>
2019-12-29 19:04:50 +0100

    select n random questions from a pool

---
+++ app/src/main/java/com/example/sarabanda/models/APIModels.kt
@@ -0,0 +1,3 @@
+package com.example.sarabanda.models
+
+data class QuestionsList (val questions: List<Question>)
--- app/src/main/java/com/example/sarabanda/models/Game.kt
+++ app/src/main/java/com/example/sarabanda/models/Game.kt
@@ -1,15 +1,22 @@
 package com.example.sarabanda.models
 
+import android.content.Context
 import com.example.sarabanda.R
+import com.example.sarabanda.utils.QuestionManager
 
 abstract class Game{
     abstract var questions: ArrayList<Question>
+
 }
 
-class ClassicGame {
-    lateinit var questions: ArrayList<ClassicGameQuestion>
+class ClassicGame (val context: Context){
+    val questions: List<ClassicGameQuestion>
+    val questionManager = QuestionManager(context)
     init {
-        questions = arrayListOf(ClassicGameQuestion(R.raw.donne, "Successo di Zucchero", "donne"))
+        val q = questionManager.getQuestions(3)
+        questions = q.map{ClassicGameQuestion(
+            0, it.suggestion, it.answer
+        )}
     }
 
 }
