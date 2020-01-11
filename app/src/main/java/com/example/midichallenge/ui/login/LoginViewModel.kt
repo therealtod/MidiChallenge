e1af52d3377d90f901159a8cb03c18d641d1ba4f
Francesco Fricano <fr.fricano@gmail.com>
2020-01-11 16:58:20 +0100

    loginActivity and SignUpActivity progress

--- app/src/main/java/com/example/midichallenge/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/midichallenge/ui/login/LoginViewModel.kt
@@ -31,9 +31,9 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
     fun loginDataChanged(username: String, password: String) {
         if (!isUserNameValid(username)) {
-            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+            _loginForm.value = LoginFormState(usernameError = R.string.login_invalid_username)
         } else if (!isPasswordValid(password)) {
-            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
+            _loginForm.value = LoginFormState(passwordError = R.string.login_invalid_password)
         } else {
             _loginForm.value = LoginFormState(isDataValid = true)
         }
